29921237b9397a7fa26c13af9e0c83145a1f6346
snylonue <snylonue@gmail.com>
2020-04-06 08:55:46 +0800

    Split parse_comment() Extract comments parsing to Comment::new() I need a better way to deal with comments

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,16 @@ use std::iter::FromIterator;
 #[derive(Debug)]
 pub struct Comment(pub Vec<String>);
 
+impl Comment {
+    pub fn new(s: &str) -> Self {
+        s.lines()
+            .filter_map(|s| parse_line(s, "REM ")
+                .ok()
+                .map(|(c, _)| c)
+            )
+            .collect()
+    }
+}
 impl<S: Into<String>> FromIterator<S> for Comment {
     fn from_iter<T: IntoIterator<Item = S>>(iter: T) -> Self {
         Self(iter.into_iter().map(Into::into).collect())
@@ -18,15 +28,9 @@ fn parse_line<'a>(line: &'a str, head: &str) -> IResult<&'a str, &'a str> {
     tag(head)(line)
 }
 fn parse_comments(s: &str) -> (Comment, String) {
-    let parse_comment = |s| parse_line(s, "REM ");
-    let comments = s.lines()
-        .filter_map(|s| parse_comment(s)
-            .ok()
-            .map(|(c, _)| c)
-        )
-        .collect::<Comment>();
+    let comments = Comment::new(s);
     let s_without_comments = s.lines()
-        .filter(|s| parse_comment(s).is_err())
+        .filter(|s| parse_line(s, "REM ").is_err())
         .collect::<Vec<&str>>()
         .join("\n");
     (comments, s_without_comments)
