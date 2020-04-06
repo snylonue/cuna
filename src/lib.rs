8ec27bf1d5e192980438d07415cfa66f2731b271
snylonue <snylonue@gmail.com>
2020-04-06 08:35:23 +0800

    Refactor Comment::FromIterator() and parse_line(), replace &[u8] with &str

--- src/lib.rs
+++ src/lib.rs
@@ -8,25 +8,25 @@ use std::iter::FromIterator;
 #[derive(Debug)]
 pub struct Comment(pub Vec<String>);
 
-impl FromIterator<String> for Comment {
-    fn from_iter<T: IntoIterator<Item = String>>(iter: T) -> Self {
-        Self(iter.into_iter().collect())
+impl<S: Into<String>> FromIterator<S> for Comment {
+    fn from_iter<T: IntoIterator<Item = S>>(iter: T) -> Self {
+        Self(iter.into_iter().map(Into::into).collect())
     }
 }
 
-fn parse_line<'a>(line: &'a [u8], head: &str) -> IResult<&'a [u8], &'a [u8]> {
+fn parse_line<'a>(line: &'a str, head: &str) -> IResult<&'a str, &'a str> {
     tag(head)(line)
 }
 fn parse_comments(s: &str) -> (Comment, String) {
     let parse_comment = |s| parse_line(s, "REM ");
     let comments = s.lines()
-        .filter_map(|s| parse_comment(s.as_bytes())
+        .filter_map(|s| parse_comment(s)
             .ok()
-            .map(|(c, _)| String::from_utf8_lossy(c).to_string()))
+            .map(|(c, _)| c)
+        )
         .collect::<Comment>();
-    let s_without_comments = s.clone()
-        .lines()
-        .filter(|s| parse_comment(s.trim().as_bytes()).is_err())
+    let s_without_comments = s.lines()
+        .filter(|s| parse_comment(s).is_err())
         .collect::<Vec<&str>>()
         .join("\n");
     (comments, s_without_comments)
