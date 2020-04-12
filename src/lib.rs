47490383dc694628e49ddf1a67fda5565bc3afac
snylonue <snylonue@gmail.com>
2020-04-12 14:11:01 +0800

    Implement parsing header It will split up the cue file first so that the command FILE can be parsed alone.

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,63 @@
+macro_rules! get {
+    ($map: expr, ($($key: ident),*) >> $map_res: expr) => {
+        [$($map.get(stringify!($key)).map($map_res),)*]
+    };
+}
+
+use failure::Error;
 use nom::IResult;
-use nom::bytes::complete::tag;
+use nom::error::ErrorKind;
+use nom::bytes::complete::tag_no_case as tag;
 use nom::bytes::complete::take_until;
+use nom::sequence::delimited;
+use nom::Err as NomErr;
 use std::fs::File;
+use std::collections::BTreeMap;
 use std::io::Read;
 use std::iter::FromIterator;
+use std::str::FromStr;
+
+type HanaResult<T> = Result<T, Error>;
 
 #[derive(Debug)]
+#[derive(Debug, Default)]
+pub struct Header {
+    pub title: Option<String>,
+    pub performer: Option<String>,
+    pub songwriter: Option<String>,
+    pub catalog: Option<u64>,
+    pub cdtextfile: Option<String>,
+}
 pub struct Comment(pub Vec<String>);
 
+impl FromStr for Header {
+    type Err = Error;
+    
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        parse_header(s)
+    }
+}
 impl Comment {
     pub fn new(s: &str) -> Self {
         s.lines()
-            .filter_map(|s| parse_line(s, "REM ")
+            .filter_map(|s| parse_line(s, "REM")
                 .ok()
-                .map(|(c, _)| c)
+                .map(|(c, _)| c.trim())
             )
             .collect()
     }
 }
 impl<S: Into<String>> FromIterator<S> for Comment {
-    fn from_iter<T: IntoIterator<Item = S>>(iter: T) -> Self {
+    fn from_iter<T: IntoIterator<Item=S>>(iter: T) -> Self {
         Self(iter.into_iter().map(Into::into).collect())
     }
 }
 
 fn parse_line<'a>(line: &'a str, head: &str) -> IResult<&'a str, &'a str> {
     tag(head)(line)
+}
+fn parse_quote(content: &str) -> IResult<&str, &str>  {
+    delimited(tag(r#"""#), take_until(r#"""#), tag(r#"""#))(content)
 }
 fn parse_comments(s: &str) -> (Comment, String) {
     let comments = Comment::new(s);
@@ -34,4 +66,51 @@ fn parse_comments(s: &str) -> (Comment, String) {
         .collect::<Vec<&str>>()
         .join("\n");
     (comments, s_without_comments)
+}
+/// Split up a cue file into `track_info` and `header`
+/// Returns a tuple of (track_info, header) (in IResult) if successful
+fn split_cue(s: &str) -> IResult<&str, String> {
+    let (mut file, headers) = take_until("FILE")(s)?;
+    let mut headers = headers.to_string();
+    let mut split_file = file.rsplitn(2, "\n  ");
+    let rest_header = split_file.next()
+        .map(|s| s.splitn(2, '\n').last())
+        .flatten();
+    match rest_header {
+        Some(rest_header) if rest_header != "" => {
+            headers += rest_header;
+            file = take_until(rest_header)(file)?.1;
+        },
+        _ => {},
+    }
+    Ok((file, headers))
+}
+fn parse_header(s: &str) -> HanaResult<Header> {
+    let mut headers: BTreeMap<&str, &str> = BTreeMap::new();
+    for line in s.lines() {
+        for command in ["title", "performer", "songwriter", "catalog", "cdtextfile"].iter() {
+            if let Ok((content, _)) = tag::<_, _, (_, ErrorKind)>(command.to_owned())(line) {
+                match parse_quote(content.trim()) {
+                    Ok((_, content)) => headers.insert(command, content),
+                    Err(NomErr::Error((content, _))) => headers.insert(command, content),
+                    _ => return Err(failure::err_msg("Unexcept error while parsing header")),
+                };
+            }
+        }
+    }
+    let [title, performer, songwriter, catalog, cdtextfile] = get!(headers,
+        (title, performer, songwriter, catalog, cdtextfile) >>
+        |s| s.to_string());
+    let header = Header {
+        title,
+        performer,
+        songwriter,
+        catalog: match catalog {
+            Some(s) if s.len() == 13 => Some(s.parse()?),
+            Some(_) => return Err(failure::err_msg("Invaild catalog")),
+            None => None,
+        },
+        cdtextfile
+    };
+    Ok(header)
 }
