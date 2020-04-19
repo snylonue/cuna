051610e0911053bd59294db1eb7f12bbffa91039
snylonue <snylonue@gmail.com>
2020-04-19 10:12:46 +0800

    lib: refactor parse_header()

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,11 @@ macro_rules! get {
         [$($map.remove(stringify!($key)).map($map_res),)*]
     };
 }
+macro_rules! tags {
+    ($($heads: expr),*) => {
+        ($(nom::bytes::complete::tag_no_case($heads),)*)
+    };
+}
 
 pub mod time;
 
@@ -15,6 +20,7 @@ use nom::error::ErrorKind;
 use nom::bytes::complete::tag_no_case as tag;
 use nom::bytes::complete::take_until;
 use nom::sequence::delimited;
+use nom::branch::alt;
 use nom::Err as NomErr;
 use std::fs::File;
 use std::collections::BTreeMap;
@@ -69,6 +75,11 @@ fn parse_line<'a>(line: &'a str, head: &str) -> IResult<&'a str, &'a str> {
 }
 fn parse_quote(content: &str) -> IResult<&str, &str>  {
     delimited(tag(r#"""#), take_until(r#"""#), tag(r#"""#))(content)
+fn quote() -> impl Fn(&str) -> IResult<&str, &str, (&str, ErrorKind)> {
+    |i: &str| delimited(tag(r#"""#), take_until(r#"""#), tag(r#"""#))(i)
+}
+fn quotec(content: &str) -> IResult<&str, &str>  {
+    quote()(content)
 }
 fn parse_comments(s: &str) -> (Comment, String) {
     let comments = Comment::new(s);
@@ -97,15 +108,14 @@ fn split_cue(s: &str) -> IResult<&str, String> {
     Ok((file, headers))
 }
 fn parse_header(s: &str) -> HanaResult<Header> {
-    let mut headers: BTreeMap<&str, _> = BTreeMap::new();
+    let mut headers = BTreeMap::new();
     for line in s.lines() {
-        for command in ["title", "performer", "songwriter", "catalog", "cdtextfile"].iter() {
-            if let Ok((content, _)) = tag::<_, _, (_, ErrorKind)>(command.to_owned())(line) {
-                match parse_quote(content.trim()) {
-                    Ok((_, content)) | Err(NomErr::Error((content, _))) => headers.entry(command).or_insert_with(|| Vec::with_capacity(1)).push(content.to_owned()),
-                    _ => return Err(failure::err_msg("Unexcept error while parsing header")),
-                };
+        match alt::<_, _, (_, ErrorKind), _>(tags!("title", "performer", "songwriter", "catalog", "cdtextfile"))(line) {
+            Ok((content, command)) => match quotec(content.trim()) {
+                Ok((_, content)) | Err(NomErr::Error((content, _))) => headers.entry(command.trim().to_ascii_lowercase()).or_insert_with(|| Vec::with_capacity(1)).push(content.to_owned()),
+                _ => return Err(failure::err_msg("Unexcept error while parsing header")),
             }
+            _ => {}
         }
     }
     let [title, performer, songwriter, catalog, cdtextfile] = get!(headers,
