3fb9ed227d9e773c94c7b28b4d586101c64cebb6
snylonue <snylonue@gmail.com>
2020-04-24 19:24:02 +0800

    lib: a better way to split a file Split by indentation numbers instead of keywords

--- src/lib.rs
+++ src/lib.rs
@@ -20,13 +20,14 @@ use nom::error::ErrorKind;
 use nom::bytes::complete::tag_no_case as tag;
 use nom::bytes::complete::take_until;
 use nom::sequence::delimited;
-use nom::branch::alt;
+use nom::character::complete::space0;
 use nom::Err as NomErr;
 use std::fs::File;
 use std::collections::BTreeMap;
 use std::io::Read;
 use std::iter::FromIterator;
 use std::str::FromStr;
+use crate::time::Duration;
 
 type HanaResult<T> = Result<T, Error>;
 
@@ -89,23 +90,48 @@ fn parse_comments(s: &str) -> (Comment, String) {
         .join("\n");
     (comments, s_without_comments)
 }
-/// Split up a cue file into `track_info` and `header`
-/// Returns a tuple of (track_info, header) (in IResult) if successful
-fn split_cue(s: &str) -> IResult<&str, String> {
-    let (mut file, headers) = take_until("FILE")(s)?;
-    let mut headers = headers.to_string();
-    let mut split_file = file.rsplitn(2, "\n  ");
-    let rest_header = split_file.next()
-        .map(|s| s.splitn(2, '\n').last())
-        .flatten();
-    match rest_header {
-        Some(rest_header) if rest_header != "" => {
-            headers += rest_header;
-            file = take_until(rest_header)(file)?.1;
-        },
-        _ => {},
+fn indentation_num(s: &str) -> usize {
+    space0::<_, (_, ErrorKind)>(s).map(|(_, o)| o.len()).unwrap()
+}
+/// Splits a str into two part according to if there's an indentation at a line
+/// Returns a tuple of them in Vecs like (no_indentation, with_indentation)
+fn scope(s: &str) -> (Vec<&str>, Vec<Vec<&str>>) {
+    if s == "" {
+        return (Vec::new(), Vec::new());
     }
-    Ok((file, headers))
+    let init_indents = indentation_num(s.lines().next().unwrap());
+    let mut lines = s.lines().enumerate();
+    let mut indexs = Vec::new();
+    while let Some((line_st, _)) = lines.find(|(_, s)| indentation_num(s) > init_indents) {
+        // line_st needs checking if it is at the first line
+        indexs.push(line_st);
+        if let Some((line_ed, _)) = lines.find(|(_, s)| indentation_num(s) == init_indents) {
+            indexs.push(line_ed);
+        }
+    }
+    let mut lines = s.lines().collect::<Vec<_>>();
+    if indexs.len() % 2 == 1 {
+        indexs.push(lines.len());
+    }
+    let mut in_scope = Vec::new();
+    let mut out_scope = Vec::new();
+    let mut indexs_iter = indexs.iter();
+    let mut current_index = 0;
+    while let Some(i) = indexs_iter.next() {
+        let rest = lines.split_off(i - current_index - 1);
+        current_index = i.clone();
+        out_scope.append(&mut lines);
+        lines = rest;
+        if let Some(j) = indexs_iter.next() {
+            let rest = lines.split_off(j - current_index + 1);
+            let mut scope = Vec::new();
+            scope.append(&mut lines);
+            in_scope.push(scope);
+            lines = rest;
+            current_index = j.clone();
+        }
+    }
+    (out_scope, in_scope)
 }
 fn parse_header(s: &str) -> HanaResult<Header> {
     let mut headers = BTreeMap::new();
