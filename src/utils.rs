62f6aecfcc4e0a9d38b8c513c20f35d25af2ce26
snylonue <snylonue@gmail.com>
2020-04-25 13:57:45 +0800

    utils: simplify code Though it is unclear

--- src/utils.rs
+++ src/utils.rs
@@ -4,6 +4,7 @@ use nom::bytes::complete::tag_no_case as tag;
 use nom::bytes::complete::take_until;
 use nom::sequence::delimited;
 use nom::character::complete::space0;
+use std::mem;
 
 pub(crate) fn parse_line<'a>(line: &'a str, head: &str) -> IResult<&'a str, &'a str> {
     tag(head)(line)
@@ -25,7 +26,7 @@ fn find_indentation<'a, I>(line: I, init_indents: usize) -> Vec<usize>
     while let Some((line_st, _)) = lines.find(|(_, s)| indentation_num(s) > init_indents) {
         // line_st needs checking if it is at the first line
         indexs.push(line_st);
-        indexs.extend(lines.find(|(_, s)| indentation_num(s) == init_indents).map(|line_ed| line_ed.0));
+        indexs.extend(lines.find(|(_, s)| indentation_num(s) == init_indents).map(|ed| ed.0));
     }
     if indexs.len() % 2 == 1 {
         indexs.push(line.clone().into_iter().count());
@@ -47,15 +48,11 @@ pub(crate) fn scope(s: &str) -> (Vec<&str>, Vec<Vec<&str>>) {
     let mut current_index = 0;
     while let Some(i) = indexs_iter.next() {
         let rest = lines.split_off(i - current_index - 1);
+        out_scope.append(&mut mem::replace(&mut lines, rest));
         current_index = i.clone();
-        out_scope.append(&mut lines);
-        lines = rest;
         if let Some(j) = indexs_iter.next() {
             let rest = lines.split_off(j - current_index + 1);
-            let mut scope = Vec::new();
-            scope.append(&mut lines);
-            in_scope.push(scope);
-            lines = rest;
+            in_scope.push(mem::replace(&mut lines, rest));
             current_index = j.clone();
         }
     }
