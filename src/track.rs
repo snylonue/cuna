e61ec5a64c76516d35a54a3ea93273dd2ae9ecf6
snylonue <snylonue@gmail.com>
2020-05-24 15:31:59 +0800

    track: implement some construction methods and setter and rename some fields

--- src/track.rs
+++ src/track.rs
@@ -14,13 +14,13 @@ use crate::utils;
 
 #[derive(Debug, Clone)]
 pub struct Index {
-    pub id: u8, // index id must between 1 and 99
+    pub id: u8, // index id must between 1 and 99, this filed should be private
     pub begin_time: Duration,
 }
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Track {
-    pub id: u8, // truck-id must between 1 and 99
-    pub track_type: String,
+    pub id: u8, // track-id must between 1 and 99
+    pub format: String,
     pub index: Vec<Index>,
     pub pregap: Option<Duration>,
     pub postgap: Option<Duration>,
@@ -37,6 +37,15 @@ pub struct TrackInfo {
     pub tracks: Vec<Track>,
 }
 
+impl Index {
+    pub fn new(id: u8, begin_time: Duration) -> Result<Self> {
+        if id <= 99 {
+            Ok(Self { id, begin_time })
+        } else {
+            Err(anyhow::format_err!("index-id must be between 1 and 99"))
+        }
+    }
+}
 impl FromStr for Index {
     type Err = Error;
 
@@ -47,6 +56,13 @@ impl FromStr for Index {
     }
 }
 impl Track {
+    pub fn new(id: u8, format: String) -> Result<Self> {
+        if id <= 99 {
+            Ok(Self { id, format, ..Self::default() })
+        } else {
+            Err(anyhow::format_err!("track-id must be between 1 and 99"))
+        }
+    }
     pub fn push_title(&mut self, title: String) {
         self.title.get_or_insert_with(|| Vec::with_capacity(1)).push(title)
     }
@@ -56,13 +72,43 @@ impl Track {
     pub fn push_songwriter(&mut self, songwriter: String) {
         self.songwriter.get_or_insert_with(|| Vec::with_capacity(1)).push(songwriter)
     }
+    pub fn push_index(&mut self, index: Index) {
+        self.index.push(index)
+    }
+    pub fn set_pregep(&mut self, pregap: Duration) -> Option<Duration> {
+        self.pregap.replace(pregap)
+    }
+    pub fn set_postgep(&mut self, postgap: Duration) -> Option<Duration> {
+        self.postgap.replace(postgap)
+    }
+    pub fn set_isrc(&mut self, isrc: String) -> Option<String> {
+        self.isrc.replace(isrc)
+    }
+    pub fn push_flag(&mut self, flag: String) {
+        self.flags.get_or_insert_with(|| Vec::with_capacity(1)).push(flag)
+    }
+    pub fn push_flags<F, S>(&mut self, flags: F)
+        where F: IntoIterator<Item = S>,
+            S: Into<String>
+    {
+        self.flags.get_or_insert_with(|| Vec::new()).extend(flags.into_iter().map(Into::into))
+    }
 }
 impl TrackInfo {
+    pub const fn new(name: String, format: String) -> Self {
+        Self::with_tracks(name, format, Vec::new())
+    }
+    pub const fn with_tracks(name: String, format: String, tracks: Vec<Track>) -> Self {
+        Self { name, format, tracks }
+    }
     pub fn last_track(&self) -> Option<&Track> {
         self.tracks.last()
     }
     pub fn last_track_mut(&mut self) -> Option<&mut Track> {
         self.tracks.last_mut()
+    }
+    pub fn push_track(&mut self , track: Track) {
+        self.tracks.push(track)
     }
 }
 
@@ -94,7 +140,7 @@ fn parse_track_lines<'a, I>(lines: I) -> Result<Track>
     let to_owned = |v: Vec<&str>| v.into_iter().map(|s| s.to_owned()).collect();
     let track = Track {
         id: id.parse()?,
-        track_type: track_type.to_owned(),
+        format: track_type.to_owned(),
         index: index,
         pregap: match pregap {
             Some(pregaps) if pregaps.len() == 1 => Some(pregaps[0].parse()?),
